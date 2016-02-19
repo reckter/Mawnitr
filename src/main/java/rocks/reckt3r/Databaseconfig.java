202f2ec2d517c6069fcb1853973f5bcb9cc38cd8
Hannes <hannes@guedelhoefer.de>
2016-02-19 20:21:36 +0100

    procfile

--- src/main/java/rocks/reckt3r/Databaseconfig.java
+++ src/main/java/rocks/reckt3r/Databaseconfig.java
@@ -1,24 +1,55 @@
 package rocks.reckt3r;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Driver;
+
 
 /**
- * Created by hannes on 01.02.16.
+ * @author Mischa Holz
  */
 @Configuration
-public class Databaseconfig {
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-        dataSourceBuilder.url("jdbc:sqlite:Mawnitr");
-        return dataSourceBuilder.build();
+    public SimpleDriverDataSource dataSource() throws URISyntaxException {
+        String url = System.getenv("DATABASE_URL");
+        if(url == null) {
+            // default. if you need a different configuration for development just
+            // set the environment variable in your runner using the same format as
+            // below
+            if(environment.acceptsProfiles("TEST")) {
+                url = "postgres://postgres:password@localhost:5432/rms4csw_test";
+            } else {
+                url = "postgres://postgres:password@localhost:5432/rms4csw";
+            }
+        }
+
+        URI dbUri = new URI(url);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = null;
+        if(dbUri.getUserInfo().split(":").length == 2) {
+            password = dbUri.getUserInfo().split(":")[1];
+        }
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        SimpleDriverDataSource driverDataSource = new SimpleDriverDataSource();
+        driverDataSource.setUrl(dbUrl);
+        driverDataSource.setUsername(username);
+        driverDataSource.setPassword(password);
+        driverDataSource.setDriverClass(Driver.class);
+
+        return driverDataSource;
     }
+
 }
--- src/main/java/rocks/reckt3r/Main.java
+++ src/main/java/rocks/reckt3r/Main.java
@@ -1,6 +1,5 @@
 package rocks.reckt3r;
 
-import me.reckter.telegram.Telegram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
